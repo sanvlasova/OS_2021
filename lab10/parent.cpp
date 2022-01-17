453c63727c86cbe063339bea8e3f34d53e020657
Alexandra Sadikova <ayusadikova@stud.etu.ru>
2022-01-17 19:09:49 +0300

    Update parent.cpp

--- lab10/parent.cpp
+++ lab10/parent.cpp
@@ -8,21 +8,18 @@ static void child_callback (EV_P_ ev_child *w, int revents) {
   ev_child_stop (EV_A_ w);
   ev_break(EV_A_ EVBREAK_ALL);
 
-  cout << "подпроцесс вернул код:" << WEXITSTATUS(w->rstatus));
-  //printf("подпроцесс вернул код: %d\n", WEXITSTATUS(w->rstatus));
+  printf("child вернул код: %d\n", WEXITSTATUS(w->rstatus));
 }
 
 static void stdin_callback (EV_P_ ev_io *w, int revents) {
   ev_io_stop(EV_A_ w);
   ev_break(EV_A_ EVBREAK_ALL);
 
-  cout << "клавиша нажата" << endl;
   //printf("\nклавиша нажата\n");
 }
 
 int main (int argc, char *argv[]) {
-  cout << "parent начала работу" << endl;
-  //printf("parent начала работу\n");
+  printf("parent начал работу\n");
 
   const char *arguments[5] = { "Param1", "Param2", "Param3", "Param4", NULL };
   const char *env[5] = { "env1", "env2", "env3", "env4", NULL };
@@ -48,13 +45,12 @@ int main (int argc, char *argv[]) {
     perror("fork");
   }
 
-  cout << "parent ждет нажатия клавиши" << endl;
-  //printf("parent программа ждет нажатия клавиши\n");
+  //printf("parent ждет нажатия клавиши\n");
 
   ev_io_init(&stdin_watcher, stdin_callback, STDIN_FILENO, EV_READ);
   ev_io_start(EV_DEFAULT_ &stdin_watcher);
   ev_run (EV_DEFAULT_ 0);
   
-  cout << "parent завершил работу" << endl;
-  //printf("мастер программа завершила работу\n");
+
+  printf("parent закончил работу\n");
 }
