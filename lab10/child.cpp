5a644deb434ded6a06ea7d59ca500af741b02d3b
Alexandra Sadikova <ayusadikova@stud.etu.ru>
2022-01-17 17:53:18 +0300

    Update child.cpp

--- lab10/child.cpp
+++ lab10/child.cpp
@@ -2,19 +2,26 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+using namespace std;
+
 int main (int argc, char *argv[], char *env[]) {
-  printf("child starts\n");
+  printf("подпрограмма начала работу\n");
  
   for(int i = 0; i < argc; i++) {
-    printf("Argument %d = %s\n", i, argv[i]);
-    printf("Env %d = %s\n", i, env[i]);
+    cout << "Аргумент:" << i << argv[i];
+    //printf("Аргумент %d : %s\n", i, argv[i]);
+    cout << "Env:" << i << env[i];
+    //printf("Env %d : %s\n", i, env[i]);
     sleep(1);
   }
 
-  printf("PID = %d\n", getpid());
-  printf("Parent PID = %d\n", getppid());
+  cout << "PID:" << getpid());
+  cout << "Parent PID:" << getppid());
+  //printf("PID : %d\n", getpid());
+  //printf("Parent PID : %d\n", getppid());
 
-  printf("child end\n");
+  //printf("подпрограмма завершила работу\n");
+  cout << "подпрограмма завершила работу" << endl;
 
-  return 123;
+  return 5;
 }
