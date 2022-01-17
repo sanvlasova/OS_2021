08b4069ca9cad68c8eaaeb0415434bb4a2b47bea
Alexandra Sadikova <ayusadikova@stud.etu.ru>
2022-01-17 18:05:13 +0300

    Update parent.cpp

--- lab10/parent.cpp
+++ lab10/parent.cpp
@@ -8,18 +8,21 @@ static void child_callback (EV_P_ ev_child *w, int revents) {
   ev_child_stop (EV_A_ w);
   ev_break(EV_A_ EVBREAK_ALL);
 
-  printf("подпроцесс вернул код: %d\n", WEXITSTATUS(w->rstatus));
+  cout << "подпроцесс вернул код:" << WEXITSTATUS(w->rstatus));
+  //printf("подпроцесс вернул код: %d\n", WEXITSTATUS(w->rstatus));
 }
 
 static void stdin_callback (EV_P_ ev_io *w, int revents) {
   ev_io_stop(EV_A_ w);
   ev_break(EV_A_ EVBREAK_ALL);
 
-  printf("\nклавиша нажата\n");
+  cout << "клавиша нажата" << endl;
+  //printf("\nклавиша нажата\n");
 }
 
 int main (int argc, char *argv[]) {
-  printf("мастер программа начала работу\n");
+  cout << "parent начала работу" << endl;
+  //printf("parent начала работу\n");
 
   const char *arguments[5] = { "Param1", "Param2", "Param3", "Param4", NULL };
   const char *env[5] = { "env1", "env2", "env3", "env4", NULL };
@@ -27,16 +30,16 @@ int main (int argc, char *argv[]) {
   ev_child child_watcher;
   ev_io	stdin_watcher;
   pid_t	pid	=	fork();
-  int slaveStatus;
+  int childStatus;
 
   if (pid == 0) {
-    if(execve("./slave", const_cast<char* const *>(arguments), const_cast<char* const *>(env)) == -1){
+    if(execve("./child", const_cast<char* const *>(arguments), const_cast<char* const *>(env)) == -1){
       perror("execve");
     }
   } else if (pid > 0) {
     printf("PID = %d\n", getpid());
     printf("Parent PID = %d\n", getppid());
-    printf("Slave PID = %d\n", pid);
+    printf("Child PID = %d\n", pid);
 
     ev_child_init (&child_watcher, child_callback, pid, 0);
     ev_child_start (EV_DEFAULT_ &child_watcher);
@@ -45,12 +48,13 @@ int main (int argc, char *argv[]) {
     perror("fork");
   }
 
-  printf("мастер программа ждет нажатия клавиши\n");
+  cout << "parent ждет нажатия клавиши" << endl;
+  //printf("parent программа ждет нажатия клавиши\n");
 
   ev_io_init(&stdin_watcher, stdin_callback, STDIN_FILENO, EV_READ);
   ev_io_start(EV_DEFAULT_ &stdin_watcher);
   ev_run (EV_DEFAULT_ 0);
   
-
-  printf("мастер программа завершила работу\n");
+  cout << "parent завершил работу" << endl;
+  //printf("мастер программа завершила работу\n");
 }
